a7c62240af5f9441ac8a7b08db1271b86716a786
Elias Häußler <e.haeussler@familie-redlich.de>
2026-03-25 10:10:32 +0100

    [FEATURE] Allow extracting child nodes from rendered ViewHelper

--- Classes/Fluid/ViewHelperInvocationResult.php
+++ Classes/Fluid/ViewHelperInvocationResult.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 
 namespace CPSIT\Typo3HandlebarsForms\Fluid;
 
+use CPSIT\Typo3HandlebarsForms\Domain;
 use TYPO3\CMS\Fluid;
 use TYPO3Fluid\Fluid as FluidStandalone;
 
@@ -34,4 +35,58 @@ final readonly class ViewHelperInvocationResult
         public mixed $content,
         public FluidStandalone\Core\ViewHelper\TagBuilder $tag = new FluidStandalone\Core\ViewHelper\TagBuilder(),
     ) {}
+
+    /**
+     * @param non-empty-string $tagName
+     * @return list<Domain\Renderable\ViewModel\ViewModel>
+     */
+    public function extractChildNodes(string $tagName): array
+    {
+        $content = $this->tag->getContent();
+        $children = [];
+
+        // Early return if no content is provided
+        if ($content === null) {
+            return [];
+        }
+
+        // Enforce UTF-8 on the rendered tag content
+        $useInternalErrors = libxml_use_internal_errors(true);
+        $html = '<?xml encoding="UTF-8">' . $content;
+
+        // Load tag content as DOMDocument
+        try {
+            $dom = new \DOMDocument();
+            $dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        } finally {
+            libxml_use_internal_errors($useInternalErrors);
+        }
+
+        // Parse nodes
+        $xpath = new \DOMXPath($dom);
+        $nodes = $xpath->query('//' . $tagName);
+
+        // Early return if queried nodes are invalid
+        if (!($nodes instanceof \DOMNodeList)) {
+            return [];
+        }
+
+        // Convert nodes to view models
+        foreach ($nodes as $node) {
+            if (!($node instanceof \DOMElement)) {
+                continue;
+            }
+
+            $tag = new FluidStandalone\Core\ViewHelper\TagBuilder($tagName, trim($node->textContent));
+
+            /** @var \DOMAttr $attribute */
+            foreach ($node->attributes as $attribute) {
+                $tag->addAttribute($attribute->name, $attribute->value);
+            }
+
+            $children[] = new Domain\Renderable\ViewModel\ViewModel($this->renderingContext, null, $tag);
+        }
+
+        return $children;
+    }
 }
