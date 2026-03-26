33f5b64e79253cb49dd96d2944cc140ba8729bb2
Elias Häußler <e.haeussler@familie-redlich.de>
2026-03-26 08:00:26 +0100

    [TASK] Add view model builder for `Fieldset`

---
+++ Classes/Domain/Renderable/ViewModel/FieldsetViewModelBuilder.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS extension "handlebars_forms".
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace CPSIT\Typo3HandlebarsForms\Domain\Renderable\ViewModel;
+
+use TYPO3\CMS\Fluid;
+use TYPO3\CMS\Form;
+use TYPO3Fluid\Fluid as FluidStandalone;
+
+/**
+ * FieldsetViewModelBuilder
+ *
+ * @author Elias Häußler <e.haeussler@familie-redlich.de>
+ * @license GPL-2.0-or-later
+ *
+ * @extends AbstractViewModelBuilder<Form\Domain\Model\FormElements\Section>
+ */
+final class FieldsetViewModelBuilder extends AbstractViewModelBuilder
+{
+    protected array $supportedTypes = [
+        'Fieldset',
+    ];
+
+    public function renderRenderable(
+        Form\Domain\Model\Renderable\RootRenderableInterface $renderable,
+        Fluid\Core\Rendering\RenderingContext $renderingContext,
+    ): ViewModel {
+        $additionalAttributes = $this->renderAdditionalAttributes($renderable, $renderingContext);
+        $className = $renderable->getProperties()['elementClassAttribute'] ?? null;
+
+        $tag = new FluidStandalone\Core\ViewHelper\TagBuilder('fieldset');
+        $tag->addAttributes($additionalAttributes);
+
+        if (is_string($className)) {
+            $tag->addAttribute('class', $className);
+        }
+
+        return new ViewModel($renderingContext, tag: $tag);
+    }
+}
