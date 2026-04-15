5dc2e2e3f55aa8a709072baa010096dbb420440e
Elias Häußler <e.haeussler@familie-redlich.de>
2026-04-15 12:06:29 +0200

    [TASK] Extend `FileUploadViewModelBuilder` to `ImageUpload` renderables

--- Classes/Domain/ViewModel/Builder/FileUploadViewModelBuilder.php
+++ Classes/Domain/ViewModel/Builder/FileUploadViewModelBuilder.php
@@ -34,6 +34,7 @@ final class FileUploadViewModelBuilder extends AbstractViewModelBuilder
 {
     protected array $supportedTypes = [
         'FileUpload',
+        'ImageUpload',
     ];
 
     public function renderRenderable(
